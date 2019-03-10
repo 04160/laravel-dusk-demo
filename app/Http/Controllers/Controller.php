cf3a20358bda773f1f8f4c707a12386e4e2c2b6b
KKadikis <k00kadikis@gmail.com>
2019-03-10 09:55:23 +0200

    Add value list

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -11,5 +12,23 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    public function listView(Request $request)
+    {
+        return view('list');
+    }
 
+    public function createValue(Request $request)
+    {
+
+    }
+
+    public function editValue(Request $request)
+    {
+
+    }
+
+    public function deleteValue(Request $request)
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+$controller = 'Controller';
+
+Route::middleware('auth')->group(function () use ($controller) {
+    Route::get('/list', "$controller@listView");
+    Route::post('/value/', "$controller@createValue");
+    Route::put('/value/{value_id}', "$controller@editValue");
+    Route::delete('/value/{value_id}', "$controller@deleteValue");
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
