c8342855dc2dbe06cf7d46b342c03c479ada10d4
KKadikis <k00kadikis@gmail.com>
2019-03-16 10:49:28 +0200

    Add session flash alert

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <div class="container">
         <div class="row">
+            @if(Session::has('error'))
+                <div class="alert alert-danger" role="alert">
+                    {{ Session::get('error') }}
+                </div>
+            @endif
+            @if(Session::has('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ Session::get('success') }}
+                </div>
+            @endif
+        </div>
+        <div class="row">
             <div class="col-sm-6">
                 <div>
                     <form method="GET">
